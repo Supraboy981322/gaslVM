0be6f68c58ebca3fbd904f13963b952ec739c572
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-18 16:36:20 -0500

    internal 'common' module

---
+++ common/helpers.zig
@@ -0,0 +1,17 @@
+pub const std = @import("std");
+
+pub fn is_num(str:[]u8) bool {
+    return for (str) |b| {
+        if ((b > '9' or b < '0') and (b != '.' and b != ',')) break false;
+    } else true;
+}
+
+pub fn cut(str:[]u8, thing:u8) ?[2][]u8 {
+    for (str, 0..) |b, i|
+        if (b == thing) return .{ str[0..i], str[if (i+1 < str.len) i+1 else i..] };
+    return null;
+}
+
+pub fn err_is_of_type(e:anyerror, comptime set:type) bool {
+    return std.mem.count(anyerror, std.meta.tags(set), &[_]anyerror{e}) > 0;
+}
