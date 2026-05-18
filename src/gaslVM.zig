36f6b7760bd37f3273bceacdf24a19ed171a4091
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-18 16:08:09 -0500

    module root

---
+++ src/gaslVM.zig
@@ -0,0 +1,26 @@
+const common = @import("common.zig");
+const value = @import("value.zig");
+const chunk = @import("chunk.zig");
+
+pub const Mode = common.Mode;
+
+pub const Define = common.Define;
+pub const DefineList = common.DefineList;
+pub const empty_define_list:DefineList = @constCast(&[_]Define{});
+
+pub const CodeByte = chunk.CodeByte;
+pub const OpCode = chunk.OpCode;
+pub const Chunk = chunk.Chunk;
+
+pub const VM = @import("vm.zig");
+pub const assembly = struct {
+    pub const Tokenizer = @import("tokenizer.zig");
+    pub const Token = @import("token.zig");
+    pub const compiler = @import("compiler.zig");
+};
+
+pub const Value = value.Value;
+pub const Ptr = value.Ptr;
+
+pub const Interp = @import("interp.zig");
+pub const InterpOpts = Interp.Opts;
