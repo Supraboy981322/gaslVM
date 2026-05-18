9e67c68c72a3c0bbd24d31ea3bb54ebacacc8316
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-18 16:05:39 -0500

    reorganize build to export module and separate cli

--- build.zig
+++ build.zig
@@ -1,22 +1,9 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    const root = b.option([]const u8, "root", "override root dir") orelse "src";
-
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const opts = b.addOptions();
-
-    //build settings
-    const bin = b.addExecutable(.{
-        .name = "gaslVM",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path(b.pathJoin(&.{ root, "main.zig"})),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-
     {
         const debug_trace = b.option(bool, "use_debug_trace", "enable debug tracing");
         opts.addOption(bool, "use_debug_trace", if (debug_trace) |t| t else false);
@@ -25,30 +12,73 @@ pub fn build(b: *std.Build) !void {
         opts.addOption(usize, "stack_size", stack_size);
     }
 
-    bin.root_module.addOptions("options", opts);
+    const mod_root = b.option([]const u8, "mod_root", "override module root dir") orelse "src";
+    const mod =try module(b, opts, target, optimize, mod_root);
+    try tests(b, opts, target, optimize, mod_root);
+    try cli(b, opts, target, optimize, mod);
 
+}
+
+fn cli(
+    b:*std.Build,
+    opts:*std.Build.Step.Options,
+    target:std.Build.ResolvedTarget,
+    optimize:?std.builtin.OptimizeMode,
+    mod:*std.Build.Module,
+) !void {
+    const cli_root = b.option([]const u8, "cli_root", "override cli root dir") orelse "cli";
+    const bin = b.addExecutable(.{
+        .name = "gaslVM",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path(b.pathJoin(&.{ cli_root, "main.zig"})),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    bin.root_module.addOptions("options", opts);
     b.installArtifact(bin);
-    //for 'zig build run'
+
     const run_bin = b.addRunArtifact(bin);
     if (b.args) |args| {
         run_bin.addArgs(args);
     }
     const run_step = b.step("run", "run the program");
     run_step.dependOn(&run_bin.step);
+}
 
-    const tests = b.addTest(.{
+fn module(
+    b:*std.Build,
+    opts:*std.Build.Step.Options,
+    target:std.Build.ResolvedTarget,
+    optimize:?std.builtin.OptimizeMode,
+    mod_root:[]const u8
+) !*std.Build.Module {
+    var mod = b.addModule("gaslVM", .{
+        .root_source_file = b.path(b.pathJoin(&.{ mod_root, "gaslVM.zig" })),
+        .target = target,
+        .optimize = optimize,
+    });
+    mod.addOptions("options", opts);
+    return mod;
+}
+
+fn tests(
+    b:*std.Build,
+    opts:*std.Build.Step.Options,
+    target:std.Build.ResolvedTarget,
+    optimize:?std.builtin.OptimizeMode,
+    mod_root:[]const u8,
+) !void {
+    const ze_tests = b.addTest(.{
         .root_module = b.addModule("tests", .{
-            .root_source_file = b.path(b.pathJoin(&.{ root, "test.zig" })),
+            .root_source_file = b.path(b.pathJoin(&.{ mod_root, "test.zig" })),
             .target = target,
             .optimize = optimize,
         }),
     });
-
-    tests.root_module.addOptions("options", opts);
-
-    const run_test = b.addRunArtifact(tests);
+    ze_tests.root_module.addOptions("options", opts);
+    const run_test = b.addRunArtifact(ze_tests);
     run_test.has_side_effects = true;
-
-    const test_step = b.step("test", "run the tests");
+    const test_step = b.step("test", "run the gaslVM tests");
     test_step.dependOn(&run_test.step);
 }
