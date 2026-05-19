4f785274e1736c1d9c9d7c8f155748c0f0ba9f61
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-19 15:23:44 -0500

    insert newline at end

--- cli/test.asm
+++ cli/test.asm
@@ -39,6 +39,20 @@ pos build_string
   hold ;length (internally remaining length)
   hold ;result pointer
 
+ push 10 ;'a'
+  push 1 take_off ;length
+    push 1
+    sub
+  take_copy ;result pointer
+    ptr_add
+    overwrite
+
+  push 1 take_off
+  push 1
+    sub
+  push 1
+    hold_off
+
   pos build_string_loop
 
     push 97 ;'a'
