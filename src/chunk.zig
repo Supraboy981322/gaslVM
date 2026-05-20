8c920d477dc2d25a2ee6ededc0988a544c10bab2
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-20 14:32:00 -0500

    placeholder instruction for cast

--- src/chunk.zig
+++ src/chunk.zig
@@ -226,6 +226,7 @@ pub const OpCode = enum(u8) {
     // TODO: these
     mutex,   //triggers a lock for VM; locking blocks until unlocked
     remove,  //removes from pointer pool
+    cast,    //casts a pointer to a value
 
 
     print, // TODO: remove (for debugging purposes)
