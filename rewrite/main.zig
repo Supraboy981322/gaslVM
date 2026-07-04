2a258b15a25a17c7ef7e96cc6f52b2fc1b346522
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-07-03 22:06:43 -0500

    move whitespace handling to state

--- rewrite/main.zig
+++ rewrite/main.zig
@@ -8,19 +8,15 @@ pub fn main(init:std.process.Init) !u8 {
 
     var reader:std.Io.Reader = .fixed(@embedFile("foo.asm"));
     const parsed = try parser.do(init.gpa, &reader);
-    if (parsed.failed()) |info| {
+    defer parsed.deinit(init.gpa);
+    if (parsed.failed()) |*info| {
         std.debug.print("parse error: {t}\n\t", .{info.err});
-        if (info.chunk) |chunk| {
-            std.debug.print("here -> |{s}| ", .{chunk});
-            init.gpa.free(chunk);
-        }
+        if (info.chunk) |chunk| std.debug.print("here -> |{s}| ", .{chunk});
         std.debug.print("(line {d})\n", .{info.state.line_num});
         return 1;
     }
-    const tokens = parsed.ok().?;
-    defer init.gpa.free(tokens);
 
-    const code = try VM.codeFromEnumSlice(init.gpa, tokens);
+    const code = try VM.codeFromEnumSlice(init.gpa, parsed.ok().?);
     defer init.gpa.free(code);
 
     try vm.do(code);
--- rewrite/parser.zig
+++ rewrite/parser.zig
@@ -24,6 +24,16 @@ pub const ParseReturn = union(enum) {
         err:ParseError,
     };
 
+    pub fn deinit(self:ParseReturn, alloc:std.mem.Allocator) void {
+        switch (self) {
+            .err => |e| {
+                @constCast(&e).state.line.deinit(alloc);
+                if (e.chunk) |chunk| alloc.free(chunk);
+            },
+            .okay => |code| alloc.free(code),
+        }
+    }
+
     pub fn fail(err:ParseError, state:State, chunk:?[]const u8) ParseReturn {
         return .{
             .err = .{
@@ -57,25 +67,91 @@ pub const State = struct {
     line_num:usize = 0,
     line_start:usize = 0,
     pos:usize = 0,
+    line:std.ArrayList(u8) = .empty,
+
+    mem:std.ArrayList(u8) = .empty,
+    alloc:std.mem.Allocator,
+
+    pub fn init(alloc:std.mem.Allocator) State {
+        return .{ .alloc = alloc };
+    }
+
+    pub fn deinit(self:*State) void {
+        self.mem.deinit(self.alloc);
+    }
 
     pub fn next(self:*State, reader:*std.Io.Reader) !u8 {
-        return self.hook(try reader.takeByte());
+        return try self.hook(try reader.takeByte());
     }
     
     pub fn toss(self:*State, reader:*std.Io.Reader) void {
         _ = self.next(reader) catch null;
     }
 
-    pub fn hook(self:*State, b:u8) u8 {
+    pub fn hook(self:*State, b:u8) !u8 {
+        try self.line.append(self.alloc, b);
         self.pos += 1;
         switch (b) {
             '\n' => {
                 self.line_num += 1;
                 self.line_start = self.pos;
+                self.line.clearAndFree(self.alloc);
             },
             else => {},
         }
         return b;
+    }
+
+    pub const InstructionReturn = union(enum) {
+        err:struct{ ParseError, []const u8 },
+        ok:?Instruction,
+
+        pub fn fail(e:ParseError, i:[]const u8) @This() {
+            return .{ .err = .{ e, i } };
+        }
+        pub fn done(i:?Instruction) @This() {
+            return .{ .ok = i };
+        }
+    };
+
+    pub fn whitespace(self:*State) !InstructionReturn {
+        if (self.mem.items.len == 0) return .done(null);
+
+        const chunk = try self.mem.toOwnedSlice(self.alloc);
+
+        if (stringToEnum(VM.defaults.instruction_set.Instruction, chunk)) |in| {
+            self.alloc.free(chunk);
+            return .done(.op(in));
+        }
+
+        if (chunk.len == 3 or chunk.len == 2) if (chunk[0] == 'r') {
+            switch (chunk.len) {
+                2 => {
+                    const reg = Instruction.register(chunk[1]) catch |e| {
+                        return .fail(e, chunk);
+                    };
+                    self.alloc.free(chunk);
+                    return .done(reg);
+                },
+                3 => {
+                    const c = chunk[1..];
+                    if (isDigit(c[0]) or isDigit(c[1]) or c[0] > '1' or c[1] > '5')
+                        return .fail(error.InvalidRegister, chunk);
+                    var n:u8 = (c[0]-'0') * 10;
+                    n += c[1]-'0';
+                    self.alloc.free(chunk);
+                    return .done(try .register(n));
+                },
+                else => unreachable,
+            }
+        };
+
+        if (std.fmt.parseInt(Word, chunk, 10)) |n| {
+            self.alloc.free(chunk);
+            return .done(.num(n));
+        } else |_| {}
+
+        return .fail(error.InvalidToken, chunk);
     }
 };
 
@@ -83,51 +159,20 @@ pub fn do(alloc:std.mem.Allocator, reader:*std.Io.Reader) !ParseReturn {
     var res:std.ArrayList(Instruction) = .empty;
     defer res.deinit(alloc);
 
-    var mem:std.ArrayList(u8) = .empty;
-    defer mem.deinit(alloc);
-
     var data:Data = try .init(alloc);
     defer data.deinit();
 
-    var state:State = .{};
+    var state:State = .init(alloc);
+    defer state.deinit();
 
     while (state.next(reader)) |b| {
-        if (std.ascii.isWhitespace(b)) {
-            if (mem.items.len == 0) continue;
-
-            const chunk = try mem.toOwnedSlice(alloc);
-
-            if (stringToEnum(VM.defaults.instruction_set.Instruction, chunk)) |in| {
-                try res.append(alloc, .op(in));
-                alloc.free(chunk);
+        if (std.ascii.isWhitespace(b)) switch (try state.whitespace()) {
+            .ok => |in| {
+                if (in) |i| try res.append(alloc, i);
                 continue;
-            }
-
-            if (chunk.len == 3 or chunk.len == 2) if (chunk[0] == 'r') {
-                switch (chunk.len) {
-                    2 => try res.append(alloc, try .register(chunk[1])),
-                    3 => {
-                        const c = chunk[1..];
-                        if (isDigit(c[0]) or isDigit(c[1]) or c[0] > '1' or c[1] > '5')
-                            return .fail(error.InvalidRegister, state, chunk);
-                        var n:u8 = (c[0]-'0') * 10;
-                        n += c[1]-'0';
-                        try res.append(alloc, try .register(n));
-                    },
-                    else => unreachable,
-                }
-                alloc.free(chunk);
-                continue;
-            };
-
-            if (std.fmt.parseInt(Word, chunk, 10)) |n| {
-                try res.append(alloc, .num(n));
-                alloc.free(chunk);
-                continue;
-            } else |_| {}
-
-            return .fail(error.InvalidToken, state, chunk);
-        }
+            },
+            .err => |e| return .fail(e[0], state, e[1]),
+        };
         if (b == ';') {
             while (reader.takeByte()) |c| {
                 if (c == '\n') break;
@@ -137,7 +182,7 @@ pub fn do(alloc:std.mem.Allocator, reader:*std.Io.Reader) !ParseReturn {
             };
             continue;
         }
-        try mem.append(alloc, b);
+        try state.mem.append(alloc, b);
     } else |err| switch (err) {
         error.EndOfStream => {},
         inline else => |e| return e,
