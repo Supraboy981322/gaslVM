babb80d1dec14477b2eea880c851f5fee5007db8
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-07-03 21:37:30 -0500

    keep state to return info on err

--- rewrite/parser.zig
+++ rewrite/parser.zig
@@ -14,19 +14,30 @@ pub const ParseError = error {
     UnexpectedEOF,
 };
 pub const ParseReturn = union(enum) {
-    err:ParseError,
+    err:Error,
     okay:Ok,
 
     pub const Ok = []Instruction;
+    pub const Error = struct {
+        state:State,
+        chunk:?[]const u8,
+        err:ParseError,
+    };
 
-    pub fn fail(err:ParseError) ParseReturn {
-        return .{ .err = err };
+    pub fn fail(err:ParseError, state:State, chunk:?[]const u8) ParseReturn {
+        return .{
+            .err = .{
+                .err = err,
+                .state = state,
+                .chunk = chunk
+            }
+        };
     }
     pub fn done(result:Ok) ParseReturn {
         return .{ .okay = result };
     }
 
-    pub fn failed(self:ParseReturn) ?ParseError {
+    pub fn failed(self:ParseReturn) ?Error {
         if (self == .err) return self.err;
         return null;
     }
@@ -36,9 +47,35 @@ pub const ParseReturn = union(enum) {
 
     pub fn unwrap(self:ParseReturn) ParseError!Ok {
         return switch (self) {
-            .err => |err| err,
+            .err => |err| err.err,
             .okay => |result| result,
         };
+    }
+};
+
+pub const State = struct {
+    line_num:usize = 0,
+    line_start:usize = 0,
+    pos:usize = 0,
+
+    pub fn next(self:*State, reader:*std.Io.Reader) !u8 {
+        return self.hook(try reader.takeByte());
+    }
+    
+    pub fn toss(self:*State, reader:*std.Io.Reader) void {
+        _ = self.next(reader) catch null;
+    }
+
+    pub fn hook(self:*State, b:u8) u8 {
+        self.pos += 1;
+        switch (b) {
+            '\n' => {
+                self.line_num += 1;
+                self.line_start = self.pos;
+            },
+            else => {},
+        }
+        return b;
     }
 };
 
@@ -52,15 +89,17 @@ pub fn do(alloc:std.mem.Allocator, reader:*std.Io.Reader) !ParseReturn {
     var data:Data = try .init(alloc);
     defer data.deinit();
 
-    while (reader.takeByte()) |b| {
+    var state:State = .{};
+
+    while (state.next(reader)) |b| {
         if (std.ascii.isWhitespace(b)) {
             if (mem.items.len == 0) continue;
 
             const chunk = try mem.toOwnedSlice(alloc);
-            defer alloc.free(chunk);
 
             if (stringToEnum(VM.defaults.instruction_set.Instruction, chunk)) |in| {
                 try res.append(alloc, .op(in));
+                alloc.free(chunk);
                 continue;
             }
 
@@ -70,29 +109,30 @@ pub fn do(alloc:std.mem.Allocator, reader:*std.Io.Reader) !ParseReturn {
                     3 => {
                         const c = chunk[1..];
                         if (isDigit(c[0]) or isDigit(c[1]) or c[0] > '1' or c[1] > '5')
-                            return .fail(error.InvalidRegister);
+                            return .fail(error.InvalidRegister, state, chunk);
                         var n:u8 = (c[0]-'0') * 10;
                         n += c[1]-'0';
                         try res.append(alloc, try .register(n));
                     },
                     else => unreachable,
                 }
+                alloc.free(chunk);
                 continue;
             };
 
             if (std.fmt.parseInt(Word, chunk, 10)) |n| {
                 try res.append(alloc, .num(n));
+                alloc.free(chunk);
                 continue;
             } else |_| {}
 
-            std.debug.print("here -> {s}\n", .{chunk});
-            return .fail(error.InvalidToken);
+            return .fail(error.InvalidToken, state, chunk);
         }
         if (b == ';') {
             while (reader.takeByte()) |c| {
                 if (c == '\n') break;
             } else |err| return switch (err) {
-                error.EndOfStream => .fail(error.UnexpectedEOF),
+                error.EndOfStream => .fail(error.UnexpectedEOF, state, null),
                 inline else => |e| e,
             };
             continue;
